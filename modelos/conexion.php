f05956cf080d15ccfdf0cfcee3ba15519fb24a5d
NLukas19 <lukasnahuel241@gmail.com>
2024-06-11 19:02:51 -0300

    Api de servidor

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -3,20 +3,20 @@
 class Conexion
 {
     const HOST = "localhost";
-    const DBNAME = "baseconstructora";
-    const USER = "root";
-    const PASSWORD = "";
+    const DBNAME = "controlstockcom_nobile";
+    const USER = "controlstockcom_nobile";
+    const PASSWORD = "Y2EpxmSp4YRSqffsU48H";
     static public function conectar()
     {
-        try {
-            $link = new PDO(
-                "mysql:host=" . self::HOST . ";dbname=" . self::DBNAME,
-                self::USER,
-                self::PASSWORD
-            );
+        try
+        {
+            $link = new PDO("mysql:host=" . self::HOST . ";dbname=" . self::DBNAME, self::USER,
+            self::PASSWORD);
             $link->exec("set names utf8");
             return $link;
-        } catch (PDOException $e) {
+        }
+        catch (PDOException $e)
+        {
             die("Error en la conexión a la base de datos: " . $e->getMessage());
         }
     }
